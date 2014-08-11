8076b8cf31fcefb0b1da99938121cf7e99f0bcc6
agaravag <agaravag@hotmail.com>
2014-08-11 10:28:41 -0700

    added jshint update

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.initConfig({
+jshint: {
+all: ['Gruntfile.js', 'server.js','index.js', 'router.js','requestHandlers.js']
+}
+});
+grunt.registerTask('default','jshint' );
+};
